57ed2b712993b2ac8b57c6800695173ab44ac20d
fasosl2 <faso.sl2@gmail.com>
2023-02-24 07:59:24 -0300

    React Hook Forms

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
-import { IconContainer,InputContainer, InputText } from "./styles"
-const Input = ({leftIcon, name,...rest}) => {
+
+import { Controller } from "react-hook-form";
+
+import { IconContainer,InputContainer, InputText, ErrorText } from "./styles"
+const Input = ({leftIcon, name, control, errorMessage, ...rest}) => {
     return(
+    <>
     <InputContainer>
         {leftIcon ? <IconContainer>{leftIcon}</IconContainer> : null}
-        <InputText {...rest}/>
+        <Controller
+        name={name}
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => <InputText {...field} {...rest}/>}
+        />
     </InputContainer>
+    {errorMessage ? <ErrorText>{errorMessage}</ErrorText> : null}
+    </>
     )
 }
 
